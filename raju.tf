9078022df9f5513bb146005ac0c87cad4d82e326
root <root@ip-172-31-58-134.ec2.internal>
2023-08-22 07:04:21 +0000

    this is my commits

---
+++ raju.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "us-west-1"  
+}
+resource "aws_s3_bucket" "rajubala1234" {
+  bucket = "testing-s3-with-terraform2000"
+  acl = "private"
+  versioning {
+       enabled = "true"
+  }
+  tags = {
+    Name = "bucket2000"
+    Environment = "Test"
+  }
